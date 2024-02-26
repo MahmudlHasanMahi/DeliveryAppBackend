e377025e80895ea95f0c36ac8780539a48657aa5
Mahmud <jakirmahi20009>
2024-02-26 21:15:10 +0600

    testing

--- backend/settings.py
+++ backend/settings.py
@@ -121,11 +121,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 
-STATIC_URL= '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles") 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
+
+
 
 
 # Database
@@ -167,8 +164,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles") 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'api.User'
